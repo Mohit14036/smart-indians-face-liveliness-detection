94e78958bbf57dde6538be79ff99dd3fd6d614c0
Mohit14036 <144369341+Mohit14036@users.noreply.github.com>
2024-09-15 22:34:07 +0530

    PlantDiseaseDetection.py

--- PlantDiseaseDetection.py
+++ PlantDiseaseDetection.py
@@ -148,7 +148,7 @@ val_acc = history.history['val_acc']
 loss = history.history['loss']
 val_loss = history.history['val_loss']
 epochs = range(1, len(acc) + 1)
-#Train and validation accuracy
+
 plt.plot(epochs, acc, 'b', label='Training accurarcy')
 plt.plot(epochs, val_acc, 'r', label='Validation accurarcy')
 plt.title('Training and Validation accurarcy')
